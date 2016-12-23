1e2243abf62f129aef254e8b1abc89245e28fd17
Dyonovan <lookinforfun0000@gmail.com>
2016-12-23 12:46:05 -0500

    Initial commit

---
+++ src/main/java/com/teambrmodding/immersiveevolution/ImmersiveEvolution.java
@@ -0,0 +1,14 @@
+package com.teambrmodding.immersiveevolution;
+
+/**
+ * This file was created for ImmersiveEvolution
+ * <p>
+ * ImmersiveEvolution is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author Dyonovan
+ * @since 12/23/2016
+ */
+public class ImmersiveEvolution {
+}
